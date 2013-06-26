8cbe9f3f8b64a64129a4acfb78a34e17c6a57edc
arun <arun@192.168.1.139>
2013-06-26 00:38:59 -0500

    added size, set color method to report interface

--- webapp/src/Report.java
+++ webapp/src/Report.java
@@ -2,5 +2,11 @@
 public interface Report {
 	
 	public void setColumnName(String columnName);
+	
+	public void setBackgroundColor(Integer colorCode);
+	
+	public ReportSize getReportsize();
+	
+	public Integer getReportTabCount();
 
 }
--- webapp/src/Report1.java
+++ webapp/src/Report1.java
@@ -7,4 +7,22 @@ public class Report1 implements Report{
 		
 	}
 
+	@Override
+	public void setBackgroundColor(Integer colorCode) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ReportSize getReportsize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getReportTabCount() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
